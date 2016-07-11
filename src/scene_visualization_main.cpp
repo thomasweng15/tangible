aced40a4570feb75b2f88fc845061ce721807b4b
Yasaman <einsian@gmail.com>
2016-07-11 16:41:46 -0700

    visualization of segmented table top w/o relying on rapid::perception::SceneViz

--- src/scene_visualization_main.cpp
+++ src/scene_visualization_main.cpp
@@ -12,7 +12,6 @@
 
 #include "rapid_utils/math.h"
 
-#include "rapid_perception/scene_viz.h"
 #include "rapid_viz/markers.h"
 
 #include "ar_track_alvar_msgs/AlvarMarkers.h"
@@ -37,12 +36,41 @@ private:
 	
 	ros::Subscriber pcl_sub;
 	ros::Publisher cloud_pub;
-	rapid::perception::SceneViz* scene_viz;
-	rapid::viz::MarkerPub* scene_pub;
+	std::vector<visualization_msgs::Marker> scene_elements;
+	ros::Publisher scene_element_pub;
 
 	ros::Subscriber ar_marker_sub;
 	visualization_msgs::Marker labels[TAG_NUM];
 	ros::Publisher label_pub;
+
+	void segmentationCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+		readPointCloud(msg);
+		bool success = parseScene();
+		if(!success) {
+			ROS_ERROR("failed to parse the scene.");
+			for(int i = 0; i < scene_elements.size(); i++) {
+				setAction(scene_elements.at(i), DELETE);
+				scene_element_pub.publish(scene_elements.at(i));
+				scene_elements.pop_back();
+			}
+		} else {
+			ROS_INFO("displaying objects.");
+			displayElements();
+		}
+		publishCloud();
+	}
+
+	void readPointCloud(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+		sensor_msgs::PointCloud2 transformed;
+		tf_listener.waitForTransform("/"+FRAME_ID, msg->header.frame_id, ros::Time(0), ros::Duration(10));
+		pcl_ros::transformPointCloud("/"+FRAME_ID, *msg, transformed, tf_listener);
+		pcl::fromROSMsg(transformed, *cloud);
+	}
+
+	bool parseScene() {
+		setParseParams();
+		return rapid::perception::ParseScene(cloud, params, &scene);
+	}
 
 	void setParseParams() {
 		params.scene.min_x = 0.2;
@@ -59,71 +87,68 @@ private:
 		params.objects.min_cluster_size = 38;
 	}
 
-	void readPointCloud() {
-		sensor_msgs::PointCloud2ConstPtr msg =
-		                 ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/cloud_in");
-		sensor_msgs::PointCloud2 transformed;
-		tf_listener.waitForTransform("/"+FRAME_ID, msg->header.frame_id, ros::Time(0), ros::Duration(10));
-		pcl_ros::transformPointCloud("/"+FRAME_ID, *msg, transformed, tf_listener);
-		pcl::fromROSMsg(transformed, *cloud);
-	}
-
-	bool parseScene() {
-		setParseParams();
-		return rapid::perception::ParseScene(cloud, params, &scene);
-	}
-
-	void displayObject() {
-		const rapid::perception::HSurface& tableTop = scene.primary_surface();
-		const std::vector<rapid::perception::Object>& objects = tableTop.objects();
-		//ROS_INFO("#Object found: %2d", (int) objects.size());
-
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr table_cloud = tableTop.GetCloud();
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr display(new pcl::PointCloud<pcl::PointXYZRGB>);
-		display->header.frame_id = FRAME_ID;
-		*display = *table_cloud;
-
-		std::stringstream ss;
-		double r, g, b;
-		for(int i = 0; i < objects.size(); i++) {
-			//ss.str(std::string());
-			const rapid::perception::Object& obj = objects[i];
-			pcl::PointCloud<pcl::PointXYZRGB>::Ptr obj_cloud = obj.GetCloud();
-			
-			// avg color of a point cloud cluster
-			r = g = b = 0;
-			for(int j = 0; j < obj_cloud->size(); j++) {
-				const pcl::PointXYZRGB& pt = obj_cloud->at(j);
-				r += pt.r; g+= pt.g; b+= pt.b;
-			}
-			r /= obj_cloud->size();
-			g /= obj_cloud->size();
-			b /= obj_cloud->size();
-			//ss << obj.name() << " size: " << obj_cloud->size() 
-			//                 << ", RGB color: " << r << ", " << g << ", " << b;
-			//ROS_INFO("scene report \t %s", ss.str().c_str());
-			*display += *obj_cloud;
-			//YSS: I don't see the colors being assigned
-		}
-		
-		*cloud = *display;
-		scene_viz = new rapid::perception::SceneViz(scene_pub); 
-		scene_viz->set_scene(scene);
-		scene_viz->Visualize();
-		if (scene_viz != NULL) {
-			ROS_INFO("deleting the scene_viz");
-			delete scene_viz;
-		}
-		//YSS: where are the pcl deleted?
-		//     they are shared pointers so pcl handles deletion itself
-	}
-
 	void publishCloud() {
-		ROS_INFO("refreshing the scene cloud");
 		sensor_msgs::PointCloud2 c;
 		pcl::toROSMsg(*cloud, c);
 		c.header.stamp = ros::Time::now();
 		cloud_pub.publish(c);
+	}
+
+	void displayElements() {
+		const rapid::perception::HSurface& table_top = scene.primary_surface();
+		
+		if(scene_elements.size() == 0) {
+			ROS_INFO("Filling scene element stack afresh...");
+			// box
+			visualization_msgs::Marker table_top_box = 
+			    rapid::viz::Marker::Box(NULL, table_top.pose(), table_top.scale()).marker();
+			setNamespace(table_top_box, "scene_visualization");
+			setID(table_top_box, 0);
+			setAction(table_top_box, ADD);
+			scene_elements.push_back(table_top_box);
+
+
+			// lable
+			visualization_msgs::Marker table_top_label = 
+			    rapid::viz::Marker::Text(NULL, table_top.pose(), "table top", 0.03).marker();
+			setNamespace(table_top_label, "scene_visualization");
+			setID(table_top_label, 1);
+			setColor(table_top_label, 1, 0, 0, 1);
+			setAction(table_top_label, ADD);
+			scene_elements.push_back(table_top_label);
+
+			// TO-DO add objects segmented on the table top
+		} else {
+			// TO-DO objects that are no longer segmented should be deleted
+			//       new objects should be added
+		}
+
+		for(int i = 0; i < scene_elements.size(); i++) {
+			scene_element_pub.publish(scene_elements.at(i));
+		}
+	}
+
+	void labelCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
+		int found[TAG_NUM];
+		for(int i = 0; i < TAG_NUM; i++)
+			found[i] = -1;
+
+		for(int i = 0; i < msg->markers.size(); i++)
+			found[msg->markers[i].id] = i;
+
+		std::stringstream ss;
+		for(int i = 0; i < TAG_NUM; i++) {
+			ss.str(std::string());
+			ss << "tag " << i;
+			if(found[i] == -1) setAction(labels[i], DELETE);
+			else {
+				geometry_msgs::PoseStamped ps = msg->markers[found[i]].pose;
+				ps.header.frame_id = msg->markers[found[i]].header.frame_id;
+				fillLabel(labels[i], ps, ss.str());
+				setAction(labels[i], ADD);
+			}
+			label_pub.publish(labels[i]);
+		}
 	}
 
 	void fillLabel(visualization_msgs::Marker& marker, geometry_msgs::PoseStamped ps, std::string txt) {
@@ -176,39 +201,21 @@ private:
 		marker.color.a = a;
 	}
 
-	void labelCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
-		int found[TAG_NUM];
-		for(int i = 0; i < TAG_NUM; i++)
-			found[i] = -1;
-
-		for(int i = 0; i < msg->markers.size(); i++)
-			found[msg->markers[i].id] = i;
-
-		std::stringstream ss;
-		for(int i = 0; i < TAG_NUM; i++) {
-			ss.str(std::string());
-			ss << "tag " << i;
-			if(found[i] == -1) setAction(labels[i], DELETE);
-			else {
-				geometry_msgs::PoseStamped ps = msg->markers[found[i]].pose;
-				ps.header.frame_id = msg->markers[found[i]].header.frame_id;
-				fillLabel(labels[i], ps, ss.str());
-				setAction(labels[i], ADD);
-			}
-			label_pub.publish(labels[i]);
-		}
-	}
-
 public:
 	Visualization(ros::NodeHandle& n) : cloud(new pcl::PointCloud<pcl::PointXYZRGB>),
-	                                    FRAME_ID("base_footprint"), tf_listener() {
+	                                    FRAME_ID("base_footprint"),
+	                                    tf_listener() {
 		node = n;
+
+		ROS_INFO("\nallow time for TF buffer to build up the TF tree...");
+		ros::Duration(10).sleep();
+
+		pcl_sub = node.subscribe("/cloud_in", 10, &Visualization::segmentationCallback, this);
 		cloud_pub = node.advertise<sensor_msgs::PointCloud2>("cloud_viz", 100);
-		scene_pub = new rapid_ros::Publisher<visualization_msgs::Marker>(
-			               node.advertise<visualization_msgs::Marker>("scene_viz", 100));
+		scene_element_pub = node.advertise<visualization_msgs::Marker>("scene_viz", 100);
+
 		ar_marker_sub = node.subscribe("/ar_pose_marker", 10, &Visualization::labelCallback, this);
 		label_pub = node.advertise<visualization_msgs::Marker>("ar_label_viz", 10);
-		//refreshScene(); NOTE: TF will not have enough time to buffer if this happens here
 		for(int i = 0; i < TAG_NUM; i++){
 			setNamespace(labels[i], "scene_visualization");
 			setID(labels[i], i);
@@ -218,37 +225,24 @@ public:
 	}
 	
 	~Visualization() { //YSS: this is not called upon Ctrl+c. Why? How to force its call?
-		delete scene_pub;
-		ROS_INFO("removing the labels (if any) upon exit...");
+
+		ROS_INFO("removing the labels and objects (if any) upon exit...");
 		for(int i = 0; i < TAG_NUM; i++) {
 			setAction(labels[i], DELETE);
 			label_pub.publish(labels[i]);
 		}
-		ROS_INFO("done.");
-	}
-
-	void refreshScene() {
-		readPointCloud();
-		bool success = parseScene();
-		if(success)
-			displayObject();
-		else {
-			ROS_ERROR("failed to parse the scene.");
-			publishCloud();
+		for(int i=0; i < scene_elements.size(); i++) {
+			setAction(scene_elements.at(i), DELETE);
+			scene_element_pub.publish(scene_elements.at(i));
 		}
+		ROS_INFO("done.");
 	}
 };
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "scene_visualization");
 	ros::NodeHandle node;
-	ros::Rate refreshRate(100);
 	Visualization viz(node);
-	ros::Duration(5).sleep();
-	while(ros::ok()) {
-		ros::spinOnce();
-		viz.refreshScene();
-		refreshRate.sleep();
-	}
+	ros::spin();
 	return 0;
 }
