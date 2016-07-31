e6462cb38195e59ce9ff467ea295ffe8eae1c328
sefyas <einsian@gmail.com>
2016-07-31 10:53:20 -0700

    constructor

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -2,4 +2,17 @@
 
 namespace tangible {
 
+Visualizer::Visualizer(ros::NodeHandler& n) {
+	node = n;
+
+	ar_label_pub = n.advertise<visualization_msgs::Marker>("ar_label_viz", 10);
+	scene_pub = n.advertise<visualization_msgs::Marker>("scene_viz", 10);
+	program_pub = n.advertise<visualization_msgs::Marker>("program_viz", 10);
+}
+
+void Visualizer::update(Program p) {
+	//
+	//program_pub.pub()
+}
+
 }
