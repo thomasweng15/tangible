90903741c7fd0728e4b9a42d15947f7825a93026
sefyas <einsian@gmail.com>
2016-07-30 20:48:45 -0700

    minor changes to make the program less verbose

--- src/tag_extractor.cpp
+++ src/tag_extractor.cpp
@@ -14,14 +14,16 @@ void TagExtractor::ARcallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr 
 	for(int i = 0; i < msg->markers.size(); i++) {
 		geometry_msgs::PoseStamped p = msg->markers[i].pose;
 		Tag t(p, msg->markers[i].id);
-		ROS_INFO("\n*** ID = %1d: X(%2.3lf, %2.3lf, %2.3lf) Y(%2.3lf, %2.3lf, %2.3lf) Z(%2.3lf, %2.3lf, %2.3lf) ***",
-			     t.getID(), t.getX().x, t.getX().y, t.getX().z,
-			                t.getY().x, t.getY().y, t.getY().z,
-			                t.getZ().x, t.getZ().y, t.getZ().z);
+		//ROS_INFO("\n*** ID = %1d: X(%2.3lf, %2.3lf, %2.3lf) Y(%2.3lf, %2.3lf, %2.3lf) Z(%2.3lf, %2.3lf, %2.3lf) ***",
+		//	     t.getID(), t.getX().x, t.getX().y, t.getX().z,
+		//	                t.getY().x, t.getY().y, t.getY().z,
+		//	                t.getZ().x, t.getZ().y, t.getZ().z);
 		tags.push_back(t);
 	}
 }
 
-std::vector<Tag> TagExtractor::get_tags() { return tags; }
+std::vector<Tag> TagExtractor::get_tags() {
+	return tags;
+}
 
 }
