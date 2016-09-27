80a2b6eee4e4b282c8e4475f17852df90f9b9867
sefyas <einsian@gmail.com>
2016-09-27 00:07:37 -0700

    labeling the tags on grouped instructions

--- include/tangible/visualizer.h
+++ include/tangible/visualizer.h
@@ -2,10 +2,13 @@
 #define TANGIBLE_VISUALIZER
 
 #include <string>
+#include <vector>
+#include <map>
 
 #include "ros/ros.h"
 #include "visualization_msgs/Marker.h"
 
+#include "tangible/tag.h"
 #include "tangible/program.h"
 #include "rapid_perception/object.h"
 
@@ -13,13 +16,14 @@ namespace tangible {
 
 class Visualizer {
 private:
+	const static int LABEL_POSITION_OFFSET = 0.02;
 	const static int ADD = 0;
 	const static int MODIFY = 1;
 	const static int DELETE = 2;
 
 	const static int TAG_NUM = 18;
-	int latest_instruction_num;
-	int latest_object_num;
+
+	std::map<int, std::string> LABELS_TXT;
 
 	ros::NodeHandle node;
 
@@ -29,11 +33,18 @@ private:
 	ros::Publisher scene_pub;
 	ros::Publisher program_pub;
 
+	std::vector<visualization_msgs::Marker> scene_elements;
+	std::vector<visualization_msgs::Marker> program_elements;
+
+	void fillGroupingMarker(visualization_msgs::Marker& marker, Instruction& ins);
+	void fillLabelMarker(visualization_msgs::Marker& marker, Tag& tag);
+
 	void setHeader(visualization_msgs::Marker& marker);
 	void setNamespace(visualization_msgs::Marker& marker, std::string name);
 	void setID(visualization_msgs::Marker& marker, int ID);
 	void setAction(visualization_msgs::Marker& marker, int action);
 	void setPose(visualization_msgs::Marker& marker, geometry_msgs::PoseStamped ps);
+	void setPose(visualization_msgs::Marker& marker, Tag& tag);
 	void setScale(visualization_msgs::Marker& marker, geometry_msgs::Vector3 scale);
 	void setColor(visualization_msgs::Marker& marker, double r, double g, double b, double a);
 	void setPoints(visualization_msgs::Marker& marker, Instruction& ins);
@@ -45,6 +56,7 @@ public:
 	void update(std::vector<rapid::perception::Object> objects);
 	//TO-DO overload update function for AR tags and scene
 	
+	void clearProgram();
 	void clearScene();
 	void clear();
 };
--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -15,47 +15,114 @@ Visualizer::Visualizer(ros::NodeHandle& n, std::string id) {
 	scene_pub = n.advertise<visualization_msgs::Marker>("scene_viz", 10);
 	program_pub = n.advertise<visualization_msgs::Marker>("program_viz", 10);
 
-	latest_instruction_num = 0;
-	latest_object_num = 0;
+	LABELS_TXT[0] = "@ POINT";
+	LABELS_TXT[1] = "THE OBJECT";
+	LABELS_TXT[2] = "IN REGION";
+	LABELS_TXT[3] = "THE OBJECTS";
+	LABELS_TXT[4] = "PAIRED";
+	LABELS_TXT[5] = "PICK SIDE";
+	LABELS_TXT[6] = "PICK TOP";
+	LABELS_TXT[7] = "PLACE";
+	LABELS_TXT[8] = "DROP";
+	LABELS_TXT[9] = "STEP #1";
+	LABELS_TXT[10] = "STEP #2";
+	LABELS_TXT[11] = "STEP #3";
+	LABELS_TXT[12] = "STEP #4";
+	LABELS_TXT[13] = "STEP #5";
+	LABELS_TXT[14] = "STEP #6";
+	LABELS_TXT[15] = "STEP #7";
 }
 
 Visualizer::~Visualizer() {}
 
 void Visualizer::update(Program p) {
+	clearProgram(); // TO-DO test if program is properly cleared and re-drawed
+
 	std::vector<Instruction> instructions = p.getInstructions();
-	latest_instruction_num = instructions.size();
-	for(int i = 0; i < latest_instruction_num; i++) {
-		visualization_msgs::Marker marker;
-		setHeader(marker);
-		setNamespace(marker, "tag_grouping");
-		setID(marker, i);
-		marker.type = visualization_msgs::Marker::LINE_STRIP;
-		setAction(marker, ADD);
-		marker.pose.orientation.w = 1;
-		geometry_msgs::Vector3 scale;
-		scale.x = 0.01; scale.y = 0.01;
-		setScale(marker, scale);
-		setColor(marker, 0.2, 1, 1, 1);
-		setPoints(marker, instructions[i]);
-		program_pub.publish(marker);
+	int marker_count = 0;
+	for(int i = 0; i < instructions.size(); i++) {
+
+		visualization_msgs::Marker grouping_marker;
+		fillGroupingMarker(grouping_marker, instructions[i]);
+		setID(grouping_marker, marker_count);
+		marker_count++;
+		program_elements.push_back(grouping_marker);
+		program_pub.publish(grouping_marker);
+		
+		visualization_msgs::Marker label_marker_selection,
+		                           label_marker_action,
+		                           label_marker_number,
+		                           label_marker_selection2nd;
+		
+		fillLabelMarker(label_marker_selection, instructions[i].selection);
+		setID(label_marker_selection, marker_count);
+		marker_count++;
+		program_elements.push_back(label_marker_selection);
+		program_pub.publish(label_marker_selection);
+
+		fillLabelMarker(label_marker_action, instructions[i].action);
+		setID(label_marker_action, marker_count);
+		marker_count++;
+		program_elements.push_back(label_marker_action);
+		program_pub.publish(label_marker_action);
+
+		fillLabelMarker(label_marker_number, instructions[i].number);
+		setID(label_marker_number, marker_count);
+		marker_count++;
+		program_elements.push_back(label_marker_number);
+		program_pub.publish(label_marker_number);
+
+		if(instructions[i].selection2nd.getID() != -1) {
+			fillLabelMarker(label_marker_selection2nd, instructions[i].selection2nd);
+			setID(label_marker_selection2nd, marker_count);
+			marker_count++;
+			program_elements.push_back(label_marker_selection2nd);
+			program_pub.publish(label_marker_selection2nd);
+		}
+
 	}
 }
 
 //TO-DO overload update function for AR tags and scene
 
 void Visualizer::update(std::vector<rapid::perception::Object> objects) {
-	if(objects.size() < latest_object_num)
-		clearScene();
+	clearScene(); // TO-DO test if scene is properly cleared and re-drawed
 
-	latest_object_num = objects.size();
-	for(int i = 0; i < latest_object_num; i++) {
+	for(int i = 0; i < objects.size(); i++) {
 		visualization_msgs::Marker box =
 	        rapid::viz::Marker::Box(NULL, objects[i].pose(), objects[i].scale()).marker();
 		setNamespace(box, "scene_visualization");
 		setID(box, i+1);
 		setColor(box, 0.5, 0, 0.5, 0.45);
+		scene_elements.push_back(box);
 		scene_pub.publish(box);
 	}
+}
+
+void Visualizer::fillGroupingMarker(visualization_msgs::Marker& marker, Instruction& ins) {
+	setHeader(marker);
+	setNamespace(marker, "tag_grouping");
+	marker.type = visualization_msgs::Marker::LINE_STRIP;
+	setAction(marker, ADD);
+	marker.pose.orientation.w = 1;
+	geometry_msgs::Vector3 scale;
+	scale.x = 0.01; scale.y = 0.01;
+	setScale(marker, scale);
+	setColor(marker, 0.2, 1, 1, 1);
+	setPoints(marker, ins);
+}
+
+void Visualizer::fillLabelMarker(visualization_msgs::Marker& marker, Tag& tag) {
+	setHeader(marker);
+	setNamespace(marker, "tag_grouping");
+	marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+	setAction(marker, ADD);
+	setPose(marker, tag);
+	geometry_msgs::Vector3 scale;
+	scale.z = 0.03;
+	setScale(marker, scale);
+	setColor(marker, 0, 0, 1, 1);
+	marker.text = LABELS_TXT[tag.getID()];
 }
 
 void Visualizer::setHeader(visualization_msgs::Marker& marker) {
@@ -91,6 +158,16 @@ void Visualizer::setAction(visualization_msgs::Marker& marker, int action) {
 void Visualizer::setPose(visualization_msgs::Marker& marker, geometry_msgs::PoseStamped ps) {
 	marker.header = ps.header;
 	marker.pose = ps.pose;
+}
+
+void Visualizer::setPose(visualization_msgs::Marker& marker, Tag& tag) {
+	marker.pose.position.x = tag.getCenter().x;
+	marker.pose.position.y = tag.getCenter().y;
+	marker.pose.position.z = tag.getCenter().z;
+	marker.pose.orientation.x = tag.getOrientation().x;
+	marker.pose.orientation.y = tag.getOrientation().y;
+	marker.pose.orientation.z = tag.getOrientation().z;
+	marker.pose.orientation.w = tag.getOrientation().w;
 }
 
 void Visualizer::setScale(visualization_msgs::Marker& marker, geometry_msgs::Vector3 scale) {
@@ -131,20 +208,25 @@ void Visualizer::setPoints(visualization_msgs::Marker& marker, Instruction& ins)
 	}
 }
 
+void Visualizer::clearProgram() {
+	for(int i = 0; i < program_elements.size(); i++) {
+		setAction(program_elements.at(i), DELETE);
+		scene_pub.publish(program_elements.at(i));
+	}
+	program_elements.clear();
+}
+
 void Visualizer::clearScene() {
-	for(int i = 0; i < latest_object_num; i++) {
-			visualization_msgs::Marker old_box;
-			setNamespace(old_box, "scene_visualization");
-			setID(old_box, i);
-			setAction(old_box, DELETE);
-			scene_pub.publish(old_box);
-		}
+	for(int i = 0; i < scene_elements.size(); i++) {
+		setAction(scene_elements.at(i), DELETE);
+		scene_pub.publish(scene_elements.at(i));
+	}
+	scene_elements.clear();
 }
 
 void Visualizer::clear() {
 	//TO-DO
 	//DELETE ALL markers
-	//use latest_instruction_num latest_object_num
 	clearScene();
 }
 
