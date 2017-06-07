eb254d8e0110e4adf4237b243004b816162df87d
Sarah Elliott <sksellio@gmail.com>
2017-06-07 11:04:49 -0700

    Updated grasp and matching object messages. Made grasping do basic collision check

--- tangible_programming/include/tangible/grasp_generator.h
+++ tangible_programming/include/tangible/grasp_generator.h
@@ -14,6 +14,7 @@
 #include "visualization_msgs/Marker.h"
 
 #include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
 
 #include "rapid_perception/scene.h"
 #include "rapid_perception/scene_parsing.h"
@@ -27,10 +28,60 @@
 #include "tangible_msgs/GetScene.h"
 #include "tangible_msgs/GetGrasps.h"
 
+#include "pcl/point_cloud.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl/point_types.h"
+#include "pcl/filters/filter.h"
+#include "pcl/common/centroid.h"
+#include "pcl/common/common.h"
+#include "pcl/common/distances.h"
+#include "pcl/common/pca.h"
+#include "pcl/filters/filter.h"
+#include "pcl/kdtree/kdtree_flann.h"
+#include "pcl/point_types.h"
+#include "pcl/search/kdtree.h"
+#include "pcl/segmentation/conditional_euclidean_clustering.h"
+#include "pcl/segmentation/extract_clusters.h"
+#include "pcl/segmentation/region_growing_rgb.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/concave_hull.h>
+#include <pcl/point_types_conversion.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_ros/impl/transforms.hpp>
+#include <pcl/common/centroid.h>
+#include <pcl/common/common.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/conditional_removal.h>
+#include <pcl/console/parse.h>
+
 #include "moveit_msgs/Grasp.h"
 
 
 namespace tangible {
+
+struct Box {
+	double min_x;
+	double min_y;
+	double min_z;
+	double max_x;
+	double max_y;
+	double max_z;
+};
 
 class GraspGenerator {
 private:
@@ -40,12 +91,31 @@ private:
 
 	ros::Publisher marker_pub;
 	std::vector<moveit_msgs::Grasp> grasps;
-	tangible_msgs::Scene scene;
+	tangible_msgs::SceneObject obj;
 	//distance from wrist to gripper on PR2
   	const static double palm_dist = 0.12;
   	const static double pre_grasp_dist = 0.15;
   	const static double post_grasp_dist = 0.15;
+  	const static int min_points_in_gripper = 50;
+    // max number of points in cluster that can intersect with fingers
+    const static int max_finger_collision_points = 7;
+    const static int max_palm_collision_points = 6;
+    // approximately half hand thickness
+    const static double half_gripper_height = 0.03;
+    // approximate distance from palm frame origin to palm surface
+    const static double dist_to_palm = 0.12;
+    // approximate distance from palm frame origin to fingertip with gripper closed
+    const static double dist_to_fingertips = 0.20;
+    // approx dist between fingers when gripper open
+    const static double gripper_palm_width = 0.08;
+    // approx height of pads of fingertips
+    const static double gripper_finger_height = 0.03;
+
+    const static double y_offset = 0.005;
   	void getGrasps();
+	bool hasCollision(moveit_msgs::Grasp grasp, sensor_msgs::PointCloud2 pc2);
+	int findPointsInBox(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, Box box, std::string frame);
+
 
 
 public:
--- tangible_programming/src/grasp_generator.cpp
+++ tangible_programming/src/grasp_generator.cpp
@@ -14,7 +14,7 @@ bool GraspGenerator::graspCallback(tangible_msgs::GetGrasps::Request& req,
                  tangible_msgs::GetGrasps::Response& res)
 {
   ROS_INFO("Got service call!");
-  scene = req.scene;
+  obj = req.object;
   getGrasps();
 	res.grasps = grasps;
   // publishMarkers(scene);
@@ -23,12 +23,8 @@ bool GraspGenerator::graspCallback(tangible_msgs::GetGrasps::Request& req,
 
 void GraspGenerator::getGrasps(){
   grasps.clear();
-  if (scene.objects.size() > 1){
-    ROS_WARN("More than one object in Scene msg given to GraspGenerator");
-    return;
-  }
 
-  tangible_msgs::SceneObject obj = scene.objects[0];
+  
 
   //pose of bounding box gives x,y position for grasp. 
   geometry_msgs::PoseStamped grasp_pose;
@@ -75,8 +71,9 @@ void GraspGenerator::getGrasps(){
   pre_vec.y = 0.0;
   pre_vec.z = 1.0;
   grasp.pre_grasp_approach.direction.vector = post_vec;
-
-  grasps.push_back(grasp);
+  if(hasCollision(grasp, obj.point_cloud) != true){
+    grasps.push_back(grasp);
+  }
 
   q = tf::createQuaternionMsgFromRollPitchYaw(0.0,3.14/2.0, yaw + 3.14/2.0);
   grasp_pose.pose.orientation.x = q.x;
@@ -84,7 +81,9 @@ void GraspGenerator::getGrasps(){
   grasp_pose.pose.orientation.z = q.z;
   grasp_pose.pose.orientation.w = q.w;
   grasp.grasp_pose = grasp_pose;
-  grasps.push_back(grasp);
+  if(hasCollision(grasp, obj.point_cloud) != true){
+    grasps.push_back(grasp);
+  }
 
 }
 
@@ -92,5 +91,124 @@ void GraspGenerator::publishMarkers(tangible_msgs::Scene scene_msg){
   ;
 }
 
+bool GraspGenerator::hasCollision(moveit_msgs::Grasp grasp, sensor_msgs::PointCloud2 pc2){
+
+
+
+
+
+    // Check if enough points will be in gripper
+    double y_offset = 0.005;
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(grasp.grasp_pose.pose.position.x,
+                                     grasp.grasp_pose.pose.position.y, 
+                                     grasp.grasp_pose.pose.position.z) );
+    tf::Quaternion q(grasp.grasp_pose.pose.orientation.x,
+                     grasp.grasp_pose.pose.orientation.y,
+                     grasp.grasp_pose.pose.orientation.z,
+                     grasp.grasp_pose.pose.orientation.w);
+    // q.setRPY(0, 0, msg->theta);
+    transform.setRotation(q);
+    std::string grasp_frame = "grasp";
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_footprint", grasp_frame));
+    ros::Duration(0.25).sleep();
+
+    // hand boxes
+    Box palm_box; 
+    Box l_finger_box; 
+    Box r_finger_box; 
+
+    Box grasp_box;
+    grasp_box = Box();
+    grasp_box.min_x = dist_to_palm;
+    grasp_box.max_x = dist_to_fingertips;
+    grasp_box.min_y = -1.0 * gripper_palm_width/2.0 + y_offset;
+    grasp_box.max_y = gripper_palm_width/2.0 + y_offset;
+    grasp_box.min_z = -1.0 * gripper_finger_height/2.0;
+    grasp_box.max_z = 1.0 * gripper_finger_height/2.0;
+
+        
+
+        // Check for collisions with fingers
+        // Left Finger
+        l_finger_box.min_x = dist_to_palm - 0.02;
+        l_finger_box.max_x = dist_to_fingertips;
+        l_finger_box.min_y = -1.0 * gripper_palm_width/2.0 - 0.025 + y_offset;
+        l_finger_box.max_y = -1.0 * gripper_palm_width/2.0 - 0.005 + y_offset;
+        l_finger_box.min_z = -1.0 * gripper_finger_height/2.0;
+        l_finger_box.max_z = gripper_finger_height/2.0;
+
+
+        // Right Finger
+        r_finger_box.min_x = dist_to_palm - 0.02;
+        r_finger_box.max_x = dist_to_fingertips;
+        r_finger_box.min_y = gripper_palm_width/2.0 + 0.005 + y_offset;
+        r_finger_box.max_y = gripper_palm_width/2.0 + 0.025 + y_offset;
+        r_finger_box.min_z = -1.0 * gripper_finger_height/2.0;
+        r_finger_box.max_z = gripper_finger_height/2.0;
+
+
+        // Check for collisions with palm
+        palm_box.min_x = 0.05;
+        palm_box.max_x = dist_to_palm - 0.01;
+        palm_box.min_y = -1.0 * gripper_palm_width/2.0 + y_offset;
+        palm_box.max_y = gripper_palm_width/2.0 + y_offset;
+        palm_box.min_z = -1.0 * gripper_finger_height/2.0;
+        palm_box.max_z = gripper_finger_height/2.0;
+
+        // ROS_INFO("got point cloud, frame_id = %s", (*msg).header.frame_id.c_str());
+        sensor_msgs::PointCloud2 transformed_cloud_msg;
+        tf::TransformListener tf_listener;
+        tf::StampedTransform cloud_transform;
+        tf_listener.waitForTransform(grasp_frame, pc2.header.frame_id, ros::Time(0), ros::Duration(3.0));
+        //ROS_INFO("... done waiting.");
+        tf_listener.lookupTransform(grasp_frame, pc2.header.frame_id, ros::Time(0), cloud_transform);
+        pcl_ros::transformPointCloud(grasp_frame, cloud_transform, pc2, transformed_cloud_msg);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+        pcl::fromROSMsg(transformed_cloud_msg, *cloud);
+
+        // Moved point cloud checking out of service call
+        int num_palm_points =  findPointsInBox(cloud, palm_box, grasp_frame);
+        int num_l_finger_points =  findPointsInBox(cloud, l_finger_box, grasp_frame);
+        int num_r_finger_points =  findPointsInBox(cloud, r_finger_box, grasp_frame);
+        // self.loginfo("Number of points inside gripper: {}".format(num_points[0]))
+        // self.loginfo("Number of points inside left finger: {}"
+        //                 .format(num_points[1]))
+        // self.loginfo("Number of points inside right finger: {}"
+        //                 .format(num_points[2]))
+        // self.loginfo("Number of points inside palm: {}"
+        //                 .format(num_points[3]))
+
+        if (num_palm_points < max_palm_collision_points && (num_r_finger_points + num_l_finger_points) < max_finger_collision_points){
+          return false;
+        }
+        else {
+          return true;
+        }
+}
+
+int GraspGenerator::findPointsInBox(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, Box box, std::string frame){
+  int num_points = 0;
+  for (size_t i = 0; i < cloud->points.size(); ++i) {
+    pcl::PointXYZRGB& point = cloud->at(i);
+    if (point.x >= box.min_x &&
+                    point.x < box.max_x &&
+                    point.y >= box.min_y &&
+                    point.y < box.max_y &&
+                    point.z >= box.min_z &&
+                    point.z < box.max_z){
+      num_points++;
+    }
+                   
+
+  }
+  ROS_INFO("Collision points: %d", num_points);
+
+
+  return num_points;
+
+      
+}
 
 }
