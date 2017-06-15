3d7fecfc8142a9b9ad4b8156f2b2cefc21a49312
Yasaman <einsian@gmail.com>
2017-06-14 18:53:18 -0700

    arm motion implementation done except for gripper control. Should be thoroughly tested once gripper control is added. Also renamed any place related variable where what was meant was both place and drop. This includes renaming class PickAndPlace to PickAndRelease as well as the associated .h and .cpp files, attempt_place to attempt_release, and constant PLACE to RELEASE.

--- tangible_programming/include/tangible/arm_motion.h
+++ tangible_programming/include/tangible/arm_motion.h
@@ -6,6 +6,9 @@
 // msg's and srv's
 #include "tangible_msgs/GetMovements.h"
 #include "tangible_msgs/StopMovements.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Vector3Stamped.h"
 #include "moveit/move_group_interface/move_group.h"
 
 namespace tangible
@@ -25,6 +28,8 @@ private:
 	// TO-DO later on also instantiate the group for left arm;
 	
 	bool status, stopped;
+
+	geometry_msgs::Point get_relative_point(geometry_msgs::Point org, geometry_msgs::Vector3Stamped vec, double mag);
 
 public:
 	ArmMotion(ros::NodeHandle& n);
--- tangible_programming/src/arm_motion.cpp
+++ tangible_programming/src/arm_motion.cpp
@@ -19,9 +19,101 @@ bool ArmMotion::move_callback(tangible_msgs::GetMovements::Request& req, tangibl
 {
 	ROS_INFO("planning motion");
 	stopped = false;
-	// TO-DO
+	status = UNSUCCESSFUL;
+
+	moveit::planning_interface::MoveGroup::Plan plan;
+
+	for(int i = 0; i < req.poses.size(); i++)
+	{
+		
+		if(stopped)
+			break;
+
+		geometry_msgs::Pose grasp, pre_grasp, post_grasp;
+		
+		grasp = req.poses[i].grasp_pose.pose;
+		
+		pre_grasp.orientation = grasp.orientation;
+		pre_grasp.position = get_relative_point(grasp.position, 
+												req.poses[i].pre_grasp_approach.direction, 
+												req.poses[i].pre_grasp_approach.desired_distance);
+		
+		post_grasp.orientation = grasp.orientation;
+		post_grasp.position = get_relative_point(grasp.position, 
+												req.poses[i].pre_grasp_approach.direction, 
+												req.poses[i].pre_grasp_approach.desired_distance);
+		
+		right_arm.setPoseTarget(pre_grasp);
+		if(!right_arm.plan(plan))
+		{
+			ROS_INFO("no plan to pre grasp");
+			continue;
+		}
+
+		right_arm.setPoseTarget(grasp);
+		if(!right_arm.plan(plan))
+		{
+			ROS_INFO("no plan to pre grasp");
+			continue;
+		}
+
+		right_arm.setPoseTarget(post_grasp);
+		if(!right_arm.plan(plan))
+		{
+			ROS_INFO("no plan to pre grasp");
+			continue;
+		}
+
+		if(stopped)
+			break;
+
+		right_arm.setPoseTarget(pre_grasp);
+		right_arm.move();
+
+		if(stopped)
+			break;
+
+		// TO-DO 
+		// - open gripper for PICK
+		// - do not do anything for RELEASE
+
+		if(stopped)
+			break;
+
+		right_arm.setPoseTarget(grasp);
+		right_arm.move();
+
+		if(stopped)
+			break;
+
+		// TO-DO 
+		// - close gripper for PICK 
+		// - open gripper for RELEASE
+
+		if(stopped)
+			break;
+
+		right_arm.setPoseTarget(post_grasp);
+		right_arm.move();
+
+		// NOTE: no need to check again whether moving to post_grasp was successful
+
+		status = SUCCESSUL;
+		break;
+
+	}
+
 	res.status = status;
 	return true;
+}
+
+geometry_msgs::Point ArmMotion::get_relative_point(geometry_msgs::Point org, geometry_msgs::Vector3Stamped vec, double mag)
+{
+	geometry_msgs::Point relative_point;
+	relative_point.x = org.x + vec.vector.x * mag;
+	relative_point.y = org.y + vec.vector.y * mag;
+	relative_point.z = org.z + vec.vector.z * mag;
+	return relative_point;
 }
 
 bool ArmMotion::stop_callback(tangible_msgs::StopMovements::Request& req, tangible_msgs::StopMovements::Response& res)
